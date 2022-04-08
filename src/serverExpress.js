fa2a7a5341c52459629674a2f18a9c2a0f284985
FlorrMammana <floramammana1994@gmail.com>
2022-04-07 23:46:03 -0300

    Cambios de trabajo

--- src/serverExpress.js
+++ src/serverExpress.js
@@ -1,7 +1,20 @@
 const express = require('express')
-
+const res = require('express/lib/response')
+const http = require('http')
 const app = express()
+const server1 = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server1)
 
-const server = app.listen(8081, () => {
-    console.log(`http://localhost:8081`)
+
+app.use(express.json())
+app.use(express.urlencoded({ extended:true}))
+app.use(express.static('./src/public'))
+server1.listen(8080, () => {
+    console.log(`http://localhost:8080`)
 })
+
+app.get('/api/productos/:id?', (req, res) => {
+    let ver = data.find(producto => producto.id === id);
+    res.json(ver);
+  });
